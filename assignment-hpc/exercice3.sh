77a02728c674b9cde0190f883f2549e07b283a6b
Basma-Arnaoui <basma.arnaoui@gmail.com>
2023-10-09 12:52:42 +0100

    ex3

---
+++ assignment-hpc/exercice3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=array_job
+#SBATCH --output=exercice3_output.txt
+#SBATCH --array=1-10%2
+
+while IFS=$'\t' read -r task_id sample_name sex; do
+    echo "This is array task $task_id, the sample name is $sample_name and the sex is $sex."
+done < config.txt
